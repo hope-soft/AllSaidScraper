a2ad94f6401067a3a9ca392efd236ee6958926db
Hope Soft <hope.soft.bd@gmail.com>
2015-06-17 15:43:11 +0600

    Update scraper.php

--- scraper.php
+++ scraper.php
@@ -1,10 +1,9 @@
 <?
 // This is a template for a PHP scraper on morph.io (https://morph.io)
 // including some code snippets below that you should find helpful
-
  require 'scraperwiki.php';
  require 'scraperwiki/simple_html_dom.php';
-  import scraperwiki           
+            
   $html = scraperwiki.scrape("http://web.archive.org/web/20110514112442/http://unstats.un.org/unsd/demographic/products/socind/education.htm");
   print $html;
 // // Read in a page
