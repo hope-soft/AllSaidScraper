c3e793dab152d4f6ae920b3dc18cb8af661bfaf1
Hope Soft <hope.soft.bd@gmail.com>
2015-06-18 10:18:34 +0600

    Update scraper.php

--- scraper.php
+++ scraper.php
@@ -1,74 +1,32 @@
 
-<?php
-require 'scraperwiki.php'; 
-date_default_timezone_set('Australia/Sydney');
-require 'scraperwiki/simple_html_dom.php';
-$mainUrl = scraperWiki::scrape("http://www.fairfieldcity.nsw.gov.au/default.asp?iNavCatID=54&iSubCatID=2240");
-$dom = new simple_html_dom();
-$dom->load($mainUrl);
-$container = $dom->find("#main table #content", 0);
-foreach($container->find("table") as $table)
-{
-    $rows = $table->find("tr");
-    print ("Number of records: " . sizeof($rows));
-    for($i = 1; $i < sizeof($rows); $i++)
-    {
-      $row = $table->find("tr", $i);
-      $record = array();
-      $cell0 = $row->find("td", 0);
-      $cell1 = $row->find("td", 1);
-      $cell2 = $row->find("td", 2);
-      $cell3 = $row->find("td", 3);
-      $cell4 = $row->find("td", 4);
-      $council_reference = trim(html_entity_decode($cell0->plaintext));
-      $record['council_reference'] = preg_replace('/[ ]/', '', $council_reference);
-      $record['address'] = trim(preg_replace('/[^a-zA-Z0-9 \.,]/', '', html_entity_decode($cell1->plaintext))) . ", NSW";
-      $description = trim(html_entity_decode($cell2->plaintext));
-      $description = preg_replace('/[^a-zA-Z0-9 \.,]/', '', $description);
-      if(strlen($description) > 228)
-      {
-  	$description = substr($description, 0, 225) . "...";
-      }
-      $record['description'] = $description;
-      $dateFromString = trim(html_entity_decode($cell3->plaintext));
-      $dateFromString = preg_replace("/&#?[a-z0-9]{2,8};/i","",$dateFromString);
-      $dateFromString = str_replace("&nbsp;", '', $dateFromString);
-      $dateFrom = date('Y-m-d', strtotime($dateFromString));
-      if($dateFrom != '1970-01-01')
-      {
-  	$record['on_notice_from'] = $dateFrom;
-      }
-      $dateToString = trim(html_entity_decode($cell4->plaintext));
-      $dateToString = preg_replace("/&#?[a-z0-9]{2,8};/i","",$dateToString);
-      $dateToString = str_replace("&nbsp;", '', $dateToString);
-      $dateTo = date('Y-m-d', strtotime($dateToString));
-      if($dateTo != '1970-01-01')
-      {
-  	$record['on_notice_to'] = $dateTo;
-      }
-      $record['info_url'] = 'http://www.fairfieldcity.nsw.gov.au/default.asp?iNavCatID=54&iSubCatID=2240';
-      $record['comment_url'] = 'http://www.fairfieldcity.nsw.gov.au/default.asp?iDocID=6779&iNavCatID=54&iSubCatID=2249';
-      $record['date_scraped'] = date('Y-m-d');
-      if($record['council_reference'] != '' && $record['description'] != '')
-      {
-          $existingRecords = scraperwiki::select("* from data where `council_reference`='" . $record['council_reference'] . "'");
-          if (count($existingRecords) == 0)
-          {
-              print ("Saving record " . $record['council_reference'] . "\n");
-              //print_r ($record);
-              scraperwiki::save(array('council_reference'), $record);
-          }
-          else
-          {
-              print ("Skipping already saved record " . $record['council_reference'] . "\n");
-          }
-      }
-      else
-      {
-          print ("Unable to save the following record:\n");
-          print_r ($record);
-      }
-    }
-}
-?>
-
+<!DOCTYPE html>           
+<html>
+    <head>
+        <title>UK Universities and Colleges</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no">
+        <meta charset="UTF-8">
+        <style type="text/css">
+            html, body, #map_canvas {
+                margin: 0;
+                padding: 0;
+                height: 100%;
+            }
+        </style>
+        <script type="text/javascript" src="https://maps.googleapis.com/maps/api/js?sensor=false"></script>
+        <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/1.7/jquery.min.js"></script>
+        <script type="text/javascript">
+            var map;
+            $(function(){
+                var myOptions = {
+                    zoom: 7,
+                    center: new google.maps.LatLng(53.405092, -2.969876),
+                    mapTypeId: google.maps.MapTypeId.ROADMAP
+                };
+                map = new google.maps.Map(document.getElementById('map_canvas'), myOptions);
+            });
+        </script>
+    </head>
+    <body>
+        <div id="map_canvas"></div>
+    </body>
+    </html>
